a4256f3158d24db9bd0d8ceefadee43e256f09d2
Nazar Soudagar <95541968+mr-nazar-05@users.noreply.github.com>
2026-02-15 01:33:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Marry-Online :: Home Page</title>
+<title>Marry-Online : Home Page</title>
 
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 function hideURLbar(){ window.scrollTo(0,1); } </script>
